becc9a184fdc6a4f0fdc39dbaa565a9853db3860
hehoku <hehoku@outlook.com>
2023-12-10 11:01:24 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+### This Repo for Learning Python
---
+++ syntax.py
@@ -0,0 +1,26 @@
+import math
+
+
+print(7 % 3)
+
+# mod = n - floor(n/d) * d
+print(-7 % 3)
+print(2 % 3)
+print(math.floor(1.9))
+
+
+mod = -5 - math.floor(-1.25) * 4
+print(mod)
+mod = -5 - (-2*4)
+print(mod)
+mod = 3
+print(mod)
+
+print(-7/3)
+print(math.floor(-2.3333333333333335))
+print(math.floor(2.3333333333333335))
+
+print('Hello World')
+
+name = 'Hehoku'
+print(f'Hello {name}')
